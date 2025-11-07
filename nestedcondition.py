034a3ae63a651bbdf74c95ba538daff3b24d0616
sarkhediya prince <sarkhediyaprince63@gmail.com>
2025-11-07 09:35:07 +0530

    princepri & nestedcondition file add

---
+++ nestedcondition.py
@@ -0,0 +1,16 @@
+x=3
+y=3
+z=4
+if(x==y):
+    if(x!=z):
+        print("x is e y")
+else:
+    print("nothing")
+
+if(x==y):
+    if(x!=z):
+        if(y==z):
+            if(z>x):
+                print("congratulation")
+else:
+    print("i + breacks on this step")
